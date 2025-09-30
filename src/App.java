765cca219c467bd0031cd535676b58809f1608d5
Sarah Bethea <sarahbetheapdx@gmail.com>
2025-09-30 12:37:32 -0600

    Setup

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // Create array[3][2][1] to represent solved cube
+
         System.out.println("Hello, World!");
     }
 }
