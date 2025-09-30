fb3bf450c0bb30a75a716998cdbba3b5f1228389
Sarah Bethea <sarahbetheapdx@gmail.com>
2025-09-30 13:06:13 -0600

    Create basic cube arrays

--- src/App.java
+++ src/App.java
@@ -1,7 +1,50 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // Create array[3][2][1] to represent solved cube
 
+        String[][] SOLVED = {
+            {"w", "w", "w",
+            "w", "w", "w",
+            "w", "w", "w"},
+
+            {"b", "b", "b",
+            "b", "b", "b",
+            "b", "b", "b"},
+
+            {"y", "y", "y", 
+            "y", "y", "y", 
+            "y", "y", "y"},
+
+            {"g", "g", "g", 
+            "g", "g", "g", 
+            "g", "g", "g"},
+
+            {"r", "r", "r", 
+            "r", "r", "r", 
+            "r", "r", "r"},
+
+            {"o", "o", "o", 
+            "o", "o", "o", 
+            "o", "o", "o"}
+        };
+
+        // Whole cube jagged array to play with
+        String[][] JAGGED_SOLVED = {
+            {"w", "w", "w", "o", "o", "o", "r", "r", "r"},
+            {"w", "w", "w", "o", "o", "o", "r", "r", "r"},
+            {"w", "w", "w", "o", "o", "o", "r", "r", "r"},
+            {"b", "b", "b"},
+            {"b", "b", "b"},
+            {"b", "b", "b"},
+            {"g", "g", "g"},
+            {"g", "g", "g"},
+            {"g", "g", "g"},
+            {"y", "y", "y"},
+            {"y", "y", "y"},
+            {"y", "y", "y"},
+        };
+
+
+    
         System.out.println("Hello, World!");
     }
 }
