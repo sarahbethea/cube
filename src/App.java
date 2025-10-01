274e8a115097770e3e76ea50a7cf9033cf065b1a
Sarah Bethea <sarahbetheapdx@gmail.com>
2025-10-01 11:13:18 -0600

    Added 3D array and print method

--- src/App.java
+++ src/App.java
@@ -1,7 +1,59 @@
 public class App {
+
+    static void printCube(String[][][] cube) {
+        for (int f = 0; f < 6; f++) {
+            for (int c = 0; c < 3; c++) {
+                for (int r = 0; r < 3; r++) {
+                    System.out.print(cube[f][c][r]);
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    
     public static void main(String[] args) throws Exception {
 
-        String[][] SOLVED = {
+        String[][][] SOLVED = {
+            {
+                {"w", "w", "w"},
+                {"w", "w", "w"},
+                {"w", "w", "w"}
+            },
+
+            {
+                {"b", "b", "b"},
+                {"b", "b", "b"},
+                {"b", "b", "b"}
+            },
+
+            {
+                {"y", "y", "y"}, 
+                {"y", "y", "y"}, 
+                {"y", "y", "y"}
+            },
+
+            {
+                {"g", "g", "g"}, 
+                {"g", "g", "g"},
+                {"g", "g", "g"}
+            }, 
+
+            {
+                {"r", "r", "r"}, 
+                {"r", "r", "r"}, 
+                {"r", "r", "r"}
+            },
+
+            {
+                {"o", "o", "o"}, 
+                {"o", "o", "o"}, 
+                {"o", "o", "o"}
+            }
+        };
+
+
+        String[][] SOLVED2D = {
             {"w", "w", "w",
             "w", "w", "w",
             "w", "w", "w"},
@@ -43,6 +95,7 @@ public class App {
             {"y", "y", "y"},
         };
 
+        printCube(SOLVED);
 
     
         System.out.println("Hello, World!");
