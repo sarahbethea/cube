25ada6782a01abc6bde31cb82df08a79aca525a7
Sarah Bethea <sarahbetheapdx@gmail.com>
2025-10-03 16:59:25 -0600

    Reformat main method

--- src/cube/Solve.java
+++ src/cube/Solve.java
@@ -1,5 +1,19 @@
 package cube;
 
+import java.util.List;
+import java.util.Stack;
+
 public class Solve {
+
+    public static List<String> solve(Stack<String> moves, List<String> solution) {
+        while (!moves.empty()) {
+            String move = moves.pop();
+            String reverse = (move.contains("'")) ? move.substring(0, 1) : move + "'";
+            System.out.print(reverse + " ");
+            solution.add(reverse);
+        }
+        return solution;
+    }
+
     
 }
